06680718d64e0abff9ca11f85e21f522c4f818f7
Oleh Kravchuk <kolegm.real@gmail.com>
2014-06-29 13:12:42 +0300

    Added module search destination with async

---
+++ test.js
@@ -0,0 +1,18 @@
+var searcher = require('./index.js');
+
+const ADDRESS = 'Paris';
+const LATITUDE = '48.833330';
+const LONGITUDE = '2.333330';
+const LANGUAGE = 'en';
+
+var options = {
+  language: LANGUAGE
+}
+
+function callback (error, result) {
+  if (error) console.log(error);
+  else console.log(result);
+}
+
+searcher.search(ADDRESS, callback, options);
+searcher.reverse(LATITUDE, LONGITUDE, callback, options);
